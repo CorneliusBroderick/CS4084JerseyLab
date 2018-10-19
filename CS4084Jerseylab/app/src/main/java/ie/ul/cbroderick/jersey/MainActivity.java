3513191b4ca7597f1b01b464631317739306e79c
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-10-19 12:05:55 +0100

    Added source image and inital name and number

--- CS4084Jerseylab/app/src/main/java/ie/ul/cbroderick/jersey/MainActivity.java
+++ CS4084Jerseylab/app/src/main/java/ie/ul/cbroderick/jersey/MainActivity.java
@@ -8,13 +8,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mNameTextView, mNumberTextView, mColour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameTextView = findViewById(R.id.name_text);
+        mNumberTextView = findViewById(R.id.number_text);
+
+
+        // Boilerplate code. Don't mess with it.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -22,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // For now just practice showing an item
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -31,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        // Boilerplate code. Don't mess with it.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -40,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
+        // TODO: Later worry about menus.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
